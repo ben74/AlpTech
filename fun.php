f3f9ef16f18d85c7ec1b5543729a8322994502e9
benjamin fontaine <benjamin.fontaine@infomaniak.com>
2022-12-24 10:51:30 +0100

    1

--- fun.php
+++ fun.php
@@ -6,7 +6,7 @@ namespace Alptech\Wip;
 
 class fun /* extends base */
 {
-    static $u,$uq,$ip,$local,$env,$t = 0, $conf = [], $_shared = [], $quotes=["'",'"'], $unquotes=["′",'″'];
+    static $u,$uq,$q,$ip,$local,$env,$t = 0, $conf = [], $_shared = [], $quotes=["'",'"'], $unquotes=["′",'″'];
 
     static function breakpoint($x)
     {
